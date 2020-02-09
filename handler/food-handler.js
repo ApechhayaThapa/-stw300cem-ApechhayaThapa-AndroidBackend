e9f9fe5dbd1edfa8c19ac861de6d78e49536fc7e
Apechhaya Thapa <thapaapechhaya@gmail.com>
2020-02-09 18:52:41 +0545

    Furniture handler Added

---
+++ handler/food-handler.js
@@ -0,0 +1,63 @@
+const path = require("path");
+const multer = require("multer");
+const client = require("../knexfile");
+const knex = require("knex")(client);
+
+const storage = multer.diskStorage({
+  destination: "./uploads/food",
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  }
+});
+
+
+function addFood(req, res) {
+  const name = req.body.name;
+  const category = req.body.category;
+  const price = req.body.price;
+  const image = req.body.image;
+
+  knex("food")
+    .insert({ name, category, price, image })
+    .then(() => res.json({ status: true, message: "Food added!!!" }))
+    .catch(err => res.json({ status: false, message: err.message }));
+}
+
+function getFood(req, res) {
+  knex("food")
+    .select()
+    .then(data => res.json({ status: true, message: "Data available.", data }))
+    .catch(err => res.json({ status: false, message: err.message }));
+}
+
+function getFoodByCategory(req, res) {
+  const category = req.params.category;
+  knex("food")
+    .select()
+    .where({ category })
+    .then(data => res.json({ status: true, message: "Data available.", data }))
+    .catch(err => res.json({ status: false, message: err.message }));
+}
+
+
+
+
+
+  knex("food")
+    .update({ name, category, price, image })
+    .where({ id })
+    .then(() => res.json({ status: true, message: "Food updated" }))
+    .catch(err => res.json({ status: false, message: err.message }));
+}
+
+module.exports = {
+  uploadFoodImage,
+  addFood,
+  getFood,
+  getFoodByCategory,
+  deleteFood,
+  updateFood
+};
